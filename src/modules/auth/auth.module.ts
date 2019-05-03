8a2c08edd261612f5e8c1d56582cf6c6ee1c868c
narek <narek.hakobyan.07@gmail.com>
2019-05-03 17:23:38 +0400

    refactor(app): jwt module form auth module to shared

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,34 +1,15 @@
-import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { Module, forwardRef } from '@nestjs/common';
 
 import { AuthController } from './auth.controller';
-import { ConfigService } from '../../shared/services/config.service';
 import { JwtStrategy } from './jwt.strategy';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
-import { SharedModule } from '../../shared.module';
 
 @Module({
     imports: [
         forwardRef(() => UserModule),
         PassportModule.register({ defaultStrategy: 'jwt' }),
-        JwtModule.registerAsync({
-            imports: [
-                SharedModule,
-            ],
-            useFactory: (configService: ConfigService) => {
-                return {
-                    secretOrPrivateKey: configService.get('JWT_SECRET_KEY'),
-                    signOptions: {
-                        expiresIn: configService.getNumber('JWT_EXPIRATION_TIME'),
-                    },
-                };
-            },
-            inject: [
-                ConfigService,
-            ],
-        }),
     ],
     controllers: [
         AuthController,
--- src/shared.module.ts
+++ src/shared.module.ts
@@ -1,4 +1,6 @@
-import { Module, Global } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { Module, Global, HttpModule } from '@nestjs/common';
+
 import { ConfigService } from './shared/services/config.service';
 import { ValidatorService } from './shared/services/validator.service';
 import { AwsS3Service } from './shared/services/aws-s3.service';
@@ -9,7 +11,25 @@ const providers = [ConfigService, ValidatorService, AwsS3Service, GeneratorServi
 @Global()
 @Module({
     providers,
-    imports: [],
-    exports: [...providers],
+    imports: [
+        HttpModule,
+        JwtModule.registerAsync({
+        imports: [
+            SharedModule,
+        ],
+        useFactory: (configService: ConfigService) => {
+            return {
+                secretOrPrivateKey: configService.get('JWT_SECRET_KEY'),
+                // if you want to use token with expiration date
+                // signOptions: {
+                //     expiresIn: configService.getNumber('JWT_EXPIRATION_TIME'),
+                // },
+            };
+        },
+        inject: [
+            ConfigService,
+        ],
+    })],
+    exports: [...providers, HttpModule, JwtModule],
 })
 export class SharedModule {}
