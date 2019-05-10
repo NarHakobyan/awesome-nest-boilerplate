44aad9151011fd12ed4e714539ebd2a56b458b53
Danny Morgan <dmorgan@mccallie.org>
2019-05-10 15:03:14 -0400

    fix(sharedmodule)
    fixed providers entry in sharedmodule

--- src/shared.module.ts
+++ src/shared.module.ts
@@ -6,30 +6,32 @@ import { ValidatorService } from './shared/services/validator.service';
 import { AwsS3Service } from './shared/services/aws-s3.service';
 import { GeneratorService } from './shared/services/generator.service';
 
-const providers = [ConfigService, ValidatorService, AwsS3Service, GeneratorService];
+const providers = [
+    ConfigService,
+    ValidatorService,
+    AwsS3Service,
+    GeneratorService,
+];
 
 @Global()
 @Module({
-    providers,
+    providers: [...providers],
     imports: [
         HttpModule,
         JwtModule.registerAsync({
-        imports: [
-            SharedModule,
-        ],
-        useFactory: (configService: ConfigService) => {
-            return {
-                secretOrPrivateKey: configService.get('JWT_SECRET_KEY'),
-                // if you want to use token with expiration date
-                // signOptions: {
-                //     expiresIn: configService.getNumber('JWT_EXPIRATION_TIME'),
-                // },
-            };
-        },
-        inject: [
-            ConfigService,
-        ],
-    })],
+            imports: [SharedModule],
+            useFactory: (configService: ConfigService) => {
+                return {
+                    secretOrPrivateKey: configService.get('JWT_SECRET_KEY'),
+                    // if you want to use token with expiration date
+                    // signOptions: {
+                    //     expiresIn: configService.getNumber('JWT_EXPIRATION_TIME'),
+                    // },
+                };
+            },
+            inject: [ConfigService],
+        }),
+    ],
     exports: [...providers, HttpModule, JwtModule],
 })
 export class SharedModule {}
