124cf9c0afe63eaf0f49e595f957c5d245bf685e
narekhakobyan <narek.hakobyan.07@gmail.com>
2019-06-01 20:40:34 +0400

    chore(auth): removed additional fields from user dto

--- src/modules/user/dto/UserDto.ts
+++ src/modules/user/dto/UserDto.ts
@@ -27,52 +27,15 @@ export class UserDto extends AbstractDto {
     avatar: string;
 
     @ApiModelPropertyOptional()
-    thumbnail: string;
-
-    @ApiModelPropertyOptional()
-    companyEmail: string;
-
-    @ApiModelPropertyOptional()
-    companyName: string;
-
-    @ApiModelPropertyOptional()
     phone: string;
-
-    @ApiModelPropertyOptional()
-    industry: string;
-
-    @ApiModelPropertyOptional()
-    address: string;
-
-    @ApiModelPropertyOptional()
-    country: string;
-
-    @ApiModelPropertyOptional()
-    state: string;
-
-    @ApiModelPropertyOptional()
-    city: string;
-
-    @ApiModelPropertyOptional()
-    zipCode: string;
 
     constructor(user: UserEntity) {
         super(user);
         this.firstName = user.firstName;
         this.lastName = user.lastName;
-        this.username = user.username;
         this.role = user.role;
         this.email = user.email;
         this.avatar = user.avatar;
-        this.thumbnail = user.thumbnail;
-        this.companyEmail = user.companyEmail;
-        this.companyName = user.companyName;
         this.phone = user.phone;
-        this.industry = user.industry;
-        this.address = user.address;
-        this.country = user.country;
-        this.state = user.state;
-        this.city = user.city;
-        this.zipCode = user.zipCode;
     }
 }
