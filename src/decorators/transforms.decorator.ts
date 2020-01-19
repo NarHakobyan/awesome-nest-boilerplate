8b331de544c6f3faeeaa606d2e67c39bcbad5e09
narek <narek.hakobyan.07@gmail.com>
2020-01-19 19:19:59 +0400

    feat(): add ToArray transformer

--- src/decorators/transforms.decorator.ts
+++ src/decorators/transforms.decorator.ts
@@ -1,8 +1,7 @@
 /* tslint:disable:naming-convention */
-'use strict';
 
-import * as _ from 'lodash';
 import { Transform } from 'class-transformer';
+import * as _ from 'lodash';
 
 /**
  * @description trim spaces from start and end, replace multiple spaces with one.
@@ -14,13 +13,14 @@ import { Transform } from 'class-transformer';
  * @returns {(target: any, key: string) => void}
  * @constructor
  */
-export const Trim = () =>
-    Transform((value: string | string[]) => {
+export function Trim() {
+    return Transform((value: string | string[]) => {
         if (_.isArray(value)) {
             return value.map(v => _.trim(v).replace(/\s\s+/g, ' '));
         }
         return _.trim(value).replace(/\s\s+/g, ' ');
     });
+}
 
 /**
  * @description convert string or number to integer
@@ -31,5 +31,26 @@ export const Trim = () =>
  * @returns {(target: any, key: string) => void}
  * @constructor
  */
-export const ToInt = () =>
-    Transform(value => parseInt(value, 10), { toClassOnly: true });
+export function ToInt() {
+    return Transform(value => parseInt(value, 10), { toClassOnly: true });
+}
+
+/**
+ * @description transforms to array, specially for query params
+ * @example
+ * @IsNumber()
+ * @ToArray()
+ * name: number;
+ * @constructor
+ */
+export function ToArray(): (target: any, key: string) => void {
+    return Transform(
+        value => {
+            if (_.isNil(value)) {
+                return [];
+            }
+            return _.castArray(value);
+        },
+        { toClassOnly: true },
+    );
+}
