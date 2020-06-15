86841f631abea7f73c0ac610fdaca68e84080d48
Khien Pham <phamkhien@hotmail.com>
2020-06-16 00:01:20 +0700

    update (#3)
    * update
    
    * update

--- src/modules/math/math.controller.ts
+++ src/modules/math/math.controller.ts
@@ -15,7 +15,7 @@ export class MathController {
     @Get('sum')
     call(): Observable<number> {
         const pattern = { cmd: 'sum' };
-        const data = [1, 2, 3, 4, 5];
+        const data = [1, 2, 3, 3333, 5];
         return this.client.send<number>(pattern, data);
     }
 
