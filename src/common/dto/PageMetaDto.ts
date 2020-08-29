310a7a5fefcacd1bf27bb1e15378613764bdae6d
narek <narek.hakobyan.07@gmail.com>
2020-08-30 00:56:32 +0400

    feat(): add hasNext and HasPrev page

--- src/common/dto/PageMetaDto.ts
+++ src/common/dto/PageMetaDto.ts
@@ -20,10 +20,18 @@ export class PageMetaDto {
     @ApiProperty()
     readonly pageCount: number;
 
+    @ApiProperty()
+    readonly hasPreviousPage: boolean;
+
+    @ApiProperty()
+    readonly hasNextPage: boolean;
+
     constructor({ pageOptionsDto, itemCount }: IPageMetaDtoParameters) {
         this.page = pageOptionsDto.page;
         this.take = pageOptionsDto.take;
         this.itemCount = itemCount;
-        this.pageCount = Math.ceil(itemCount / this.take);
+        this.pageCount = Math.ceil(this.itemCount / this.take);
+        this.hasPreviousPage = this.page > 1;
+        this.hasNextPage = this.page < this.pageCount;
     }
 }
