a556ba2762552014ad740550196cf65aac3c6e4a
Narek <narek.hakobyan.07@gmail.com>
2021-06-14 23:58:02 +0400

    fix(): fix auth guard

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -1,17 +1,18 @@
 import type { CanActivate, ExecutionContext } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import _ from 'lodash';
 
 import type { UserEntity } from '../modules/user/user.entity';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private readonly _reflector: Reflector) {}
+  constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const roles = this._reflector.get<string[]>('roles', context.getHandler());
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
 
-    if (!roles) {
+    if (_.isEmpty(roles)) {
       return true;
     }
 
