f03ef485279a830239c8bcd031f4737ef3d1d478
kazik <antenna@antenna.io>
2021-09-19 23:27:43 +0200

    fix(): e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call */
 import { Test } from '@nestjs/testing';
 import request from 'supertest';
-
+import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
 
 describe('AuthController (e2e)', () => {
-    let app;
+    let app: INestApplication;
+    let accessToken: string;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         const moduleFixture = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
@@ -16,9 +17,34 @@ describe('AuthController (e2e)', () => {
         await app.init();
     });
 
-    it('/ (GET)', () =>
-        request(app.getHttpServer())
-            .get('/')
-            .expect(200)
-            .expect('Hello World!'));
+    it('/auth/register (POST)', async () =>
+        await request(app.getHttpServer())
+            .post('/auth/register')
+            .send({
+                firstName: 'John',
+                lastName: 'Smith',
+                email: 'john@smith.com',
+                password: 'password'
+            })
+            .expect(200));
+
+    it('/auth/login (POST)', async () => {
+        let response = await request(app.getHttpServer())
+            .post('/auth/login')
+            .send({
+                email: 'john@smith.com',
+                password: 'password'
+            })
+            .expect(200);
+
+        accessToken = response.body.token.accessToken;
+    });
+
+    it('/auth/me (GET)', async () =>
+        await request(app.getHttpServer())
+            .get('/auth/me')
+            .set({ Authorization: `Bearer ${accessToken}` })
+            .expect(200));
+
+    afterAll(async () => await app.close());
 });
