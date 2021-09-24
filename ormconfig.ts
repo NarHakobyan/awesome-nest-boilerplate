9b3841418af1b09f8448ec1eb502bb1ffbb3719e
Narek <narek.hakobyan.07@gmail.com>
2021-09-24 15:58:33 +0400

    fix(): fix ormconfig error

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,22 +1,24 @@
 import './src/boilerplate.polyfill';
 
-import { SnakeNamingStrategy } from './src/snake-naming.strategy';
-import { UserSubscriber } from './src/entity-subscribers/user-subscriber';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-const configs: TypeOrmModuleOptions & {seeds: string[], factories: string[],} =  {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  namingStrategy: new SnakeNamingStrategy(),
-  subscribers: [UserSubscriber],
-  entities: ['src/modules/**/*.entity{.ts,.js}'],
-  migrations: ['src/database/migrations/*{.ts,.js}'],
-  seeds: ['src/database/seeds/**/*{.ts,.js}'],
-  factories: ['src/database/factories/**/*{.ts,.js}'],
-};
+import { UserSubscriber } from './src/entity-subscribers/user-subscriber';
+import { SnakeNamingStrategy } from './src/snake-naming.strategy';
+
+const configs: TypeOrmModuleOptions & { seeds: string[]; factories: string[] } =
+  {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    namingStrategy: new SnakeNamingStrategy(),
+    subscribers: [UserSubscriber],
+    entities: ['src/modules/**/*.entity{.ts,.js}'],
+    migrations: ['src/database/migrations/*{.ts,.js}'],
+    seeds: ['src/database/seeds/**/*{.ts,.js}'],
+    factories: ['src/database/factories/**/*{.ts,.js}'],
+  };
 
 module.exports = configs;
