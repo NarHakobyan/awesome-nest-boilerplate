1b19e81f349dbf511bd38b4f5ee6fd1465e916e3
Narek <narek.hakobyan.07@gmail.com>
2021-10-06 16:29:33 +0400

    fix(): fix faker error

--- src/database/factories/user.factory.ts
+++ src/database/factories/user.factory.ts
@@ -1,11 +1,10 @@
-import type * as Faker from 'faker';
 import { define } from 'typeorm-seeding';
 
 import { RoleType } from '../../common/constants/role-type';
 import { UserEntity } from '../../modules/user/user.entity';
 
-define(UserEntity, (faker: typeof Faker) => {
-  const gender = faker.datatype.number(1);
+define(UserEntity, (faker) => {
+  const gender = faker.random.number(1);
   const firstName = faker.name.firstName(gender);
   const lastName = faker.name.lastName(gender);
   const email = faker.internet.email(firstName, lastName);
