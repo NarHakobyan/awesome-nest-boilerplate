17dbaf2886e5722aa64870b4cb78f34d9afd4fa9
Narek Hakobyan <narek.hakobyan.07@gmail.com>
2022-07-11 15:16:24 +0400

    fix(): add SameAs function implementation

--- src/modules/auth/dto/UserLoginDto.ts
+++ src/modules/auth/dto/UserLoginDto.ts
@@ -1,9 +1,14 @@
 import { EmailField, PasswordField } from '../../../decorators';
+import { SameAs } from '../../../validators/same-as.validator';
 
 export class UserLoginDto {
   @EmailField()
   readonly email!: string;
 
   @PasswordField()
+  @SameAs('confirmPassword')
   readonly password!: string;
+
+  @PasswordField()
+  readonly confirmPassword!: string;
 }
