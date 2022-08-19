70c53a8afb7bc27bc2e87a07f7e31b385fda04dd
Narek Hakobyan <narek.hakobyan.07@gmail.com>
2022-08-19 11:24:40 +0400

    fix(): remove all-exceptions filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import './boilerplate.polyfill';
 
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { I18nModule } from 'nestjs-i18n';
 import path from 'path';
 
-import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 import { AuthModule } from './modules/auth/auth.module';
 import { HealthCheckerModule } from './modules/health-checker/health-checker.module';
 import { PostModule } from './modules/post/post.module';
@@ -43,11 +41,6 @@ import { SharedModule } from './shared/shared.module';
     }),
     HealthCheckerModule,
   ],
-  providers: [
-    {
-      provide: APP_FILTER,
-      useClass: AllExceptionsFilter,
-    },
-  ],
+  providers: [],
 })
 export class AppModule {}
