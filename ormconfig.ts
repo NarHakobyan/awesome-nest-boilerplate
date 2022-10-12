4abad9a76630d274b2aff9ce48542007ad3dae43
Narek Hakobyan <narek.hakobyan.07@gmail.com>
2022-10-13 00:20:07 +0400

    fix(): fix orm config, close #291

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,27 +1,25 @@
 import './src/boilerplate.polyfill';
 
-import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
 
 import { UserSubscriber } from './src/entity-subscribers/user-subscriber';
 import { SnakeNamingStrategy } from './src/snake-naming.strategy';
 
-const configs: TypeOrmModuleOptions & { seeds: string[]; factories: string[] } =
-  {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    namingStrategy: new SnakeNamingStrategy(),
-    subscribers: [UserSubscriber],
-    entities: [
-      'src/modules/**/*.entity{.ts,.js}',
-      'src/modules/**/*.view-entity{.ts,.js}',
-    ],
-    migrations: ['src/database/migrations/*{.ts,.js}'],
-    seeds: ['src/database/seeds/**/*{.ts,.js}'],
-    factories: ['src/database/factories/**/*{.ts,.js}'],
-  };
+dotenv.config();
 
-module.exports = configs;
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  namingStrategy: new SnakeNamingStrategy(),
+  subscribers: [UserSubscriber],
+  entities: [
+    'src/modules/**/*.entity{.ts,.js}',
+    'src/modules/**/*.view-entity{.ts,.js}',
+  ],
+  migrations: ['src/database/migrations/*{.ts,.js}'],
+});
