08e53eb80da9502f4a31c7ce07e47b389cffde33
Narek Hakobyan <narek.hakobyan.07@gmail.com>
2023-03-18 19:42:26 +0400

    chore(): upgrade libs 🚀

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Awesome NestJS Boilerplate v8
+# Awesome NestJS Boilerplate v9
 
 [![Awesome NestJS](https://img.shields.io/badge/Awesome-NestJS-blue.svg?longCache=true&style=flat-square)](https://github.com/juliandavidmr/awesome-nestjs)
 
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,40 +1,25 @@
-import { HttpModule } from '@nestjs/axios';
+import type { Provider } from '@nestjs/common';
 import { Global, Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 
-// import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { ApiConfigService } from './services/api-config.service';
 import { AwsS3Service } from './services/aws-s3.service';
 import { GeneratorService } from './services/generator.service';
 import { TranslationService } from './services/translation.service';
 import { ValidatorService } from './services/validator.service';
 
-const providers = [
+const providers: Provider[] = [
   ApiConfigService,
   ValidatorService,
   AwsS3Service,
   GeneratorService,
   TranslationService,
-  // {
-  //   provide: 'NATS_SERVICE',
-  //   useFactory: (configService: ApiConfigService) => {
-  //     const natsConfig = configService.natsConfig;
-  //     return ClientProxyFactory.create({
-  //       transport: Transport.NATS,
-  //       options: {
-  //         name: 'NATS_SERVICE',
-  //         url: `nats://${natsConfig.host}:${natsConfig.port}`,
-  //       },
-  //     });
-  //   },
-  //   inject: [ApiConfigService],
-  // },
 ];
 
 @Global()
 @Module({
   providers,
-  imports: [HttpModule, CqrsModule],
-  exports: [...providers, HttpModule, CqrsModule],
+  imports: [CqrsModule],
+  exports: [...providers, CqrsModule],
 })
 export class SharedModule {}
